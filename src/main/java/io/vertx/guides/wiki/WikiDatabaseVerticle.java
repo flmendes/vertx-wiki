59979f53740348fd2721545fbd7477e2c7225a4b
Flavio Mendes <flmendes@gmail.com>
2017-10-08 18:42:14 -0300

    commit initial

---
+++ src/main/java/io/vertx/guides/wiki/WikiDatabaseVerticle.java
@@ -0,0 +1,19 @@
+package io.vertx.guides.wiki;
+
+import io.vertx.core.AbstractVerticle;
+import io.vertx.core.logging.Logger;
+import io.vertx.core.logging.LoggerFactory;
+
+public class WikiDatabaseVerticle extends AbstractVerticle {
+
+  public static final String CONFIG_WIKIDB_JDBC_URL = "wikidb.jdbc.url";
+  public static final String CONFIG_WIKIDB_JDBC_DRIVER_CLASS = "wikidb.jdbc.driver_class";
+  public static final String CONFIG_WIKIDB_JDBC_MAX_POOL_SIZE = "wikidb.jdbc.max_pool_size";
+  public static final String CONFIG_WIKIDB_SQL_QUERIES_RESOURCE_FILE = "wikidb.sqlqueries.resource.file";
+
+  public static final String CONFIG_WIKIDB_QUEUE = "wikidb.queue";
+
+  private static final Logger LOGGER = LoggerFactory.getLogger(WikiDatabaseVerticle.class);
+
+
+}
